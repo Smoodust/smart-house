2a4495fede962be10f7805660f4341133f1aa7bd
Pluton <rustyburing@yandex.ru>
2026-03-29 00:04:07 +0400

    Remade WeatherRepository

--- src/main/java/ru/tbank/practicum/repository/entity/WeatherLocation.java
+++ src/main/java/ru/tbank/practicum/repository/entity/WeatherLocation.java
@@ -1,32 +1,32 @@
 package ru.tbank.practicum.repository.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Entity
 @Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class WeatherLocation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "weather_id")
+    private Long id;
+
+    @Column(name = "latitude")
     private double latitude;
+
+    @Column(name = "longtitude")
     private double longtitude;
+
+    @Column(name = "temperature")
     private double temperature;
-
-    public WeatherLocation(double latitude, double longtitude, double temperature) {
-        this.latitude = latitude;
-        this.longtitude = longtitude;
-        this.temperature = temperature;
-    }
-
-    public void setTemperature(double temperature) {
-        this.temperature = temperature;
-    }
-
-    public double getLatitude() {
-        return latitude;
-    }
-
-    public double getLongtitude() {
-        return longtitude;
-    }
-
-    public double getTemperature() {
-        return temperature;
-    }
 }
--- src/main/java/ru/tbank/practicum/service/WeatherScheduler.java
+++ src/main/java/ru/tbank/practicum/service/WeatherScheduler.java
@@ -1,12 +1,13 @@
 package ru.tbank.practicum.service;
 
-import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import ru.tbank.practicum.repository.WeatherRepository;
 import ru.tbank.practicum.repository.entity.WeatherLocation;
-import ru.tbank.practicum.service.dto.WeatherAPIResponse;
 
 @Service
 public class WeatherScheduler {
@@ -18,16 +19,11 @@ public class WeatherScheduler {
 
     @Scheduled(cron = "0 0 * * * *")
     public void updateWeatherLocationInfo() {
-        for (WeatherLocation wl : weatherRepository.getAllWeatherLocations()) {
-            Optional<WeatherAPIResponse> weatherResponse =
-                    Optional.ofNullable(weatherService.getWeather(wl.getLatitude(), wl.getLongtitude()));
-            weatherResponse
-                    .map(WeatherAPIResponse::main)
-                    .map(WeatherAPIResponse.Main::temp)
-                    .ifPresent(temp -> {
-                        WeatherLocation newWeather = new WeatherLocation(wl.getLatitude(), wl.getLongtitude(), temp);
-                        weatherRepository.updateWeather(newWeather);
-                    });
+        Pageable page = PageRequest.of(0, 100);
+        Page<WeatherLocation> result = weatherRepository.findAll(page);
+        while (result.hasContent()) {
+            result.stream().map(wl -> weatherService.updateWeather(wl));
+            page = page.next();
         }
     }
 }
