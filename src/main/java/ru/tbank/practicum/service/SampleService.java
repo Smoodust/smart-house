4f0852ee3a2688edeca9cfa5a23dfc854c4073ab
Pluton <rustyburing@yandex.ru>
2026-04-16 10:38:07 +0400

    Added Jacoco with sample test

---
+++ src/main/java/ru/tbank/practicum/service/SampleService.java
@@ -0,0 +1,10 @@
+package ru.tbank.practicum.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class SampleService {
+    public int add(int x, int y) {
+        return x + y;
+    }
+}
